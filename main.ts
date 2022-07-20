da8970e3bd7bc87c226bfa02c2b9141c344fe3f1
Charlie <64992071+22cnorton@users.noreply.github.com>
2022-07-20 11:07:28 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,11 +17,11 @@ input.onButtonPressed(Button.A, function () {
 })
 function drawSnek (newApplePos: boolean) {
     basic.clearScreen()
-    led.plotBrightness(appleX, appleY, 128)
-    led.plotBrightness(snekXPos[0], snekYPos[0], 255)
     for (let index = 0; index <= snekLength - 2; index++) {
         led.plotBrightness(snekXPos[index + 1], snekYPos[index + 1], 56)
     }
+    led.plotBrightness(appleX, appleY, 128)
+    led.plotBrightness(snekXPos[0], snekYPos[0], 255)
     if (newApplePos) {
         music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 374, 1578, 255, 220, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     } else {
@@ -36,19 +36,28 @@ function contains (a: any[], obj: number) {
     }
     return false
 }
+function snakeColidingWithBody (xPos: any[], yPos: number[], headX: number, headY: number) {
+    for (let index = 0; index <= xPos.length; index++) {
+        if (xPos[index] == headX && yPos[index] == headY) {
+            return true
+        }
+    }
+    return false
+}
 input.onButtonPressed(Button.B, function () {
     snekDirection += 90
     if (snekDirection > 360) {
         snekDirection += -360
     }
 })
+let hit_wall = false
 let newAppPos = false
 let snekLength = 0
 let snekDirection = 0
-let snekXPos: number[] = []
-let snekYPos: number[] = []
-let appleY = 0
 let appleX = 0
+let appleY = 0
+let snekYPos: number[] = []
+let snekXPos: number[] = []
 startGame()
 basic.forever(function () {
     if (snekDirection == 90) {
@@ -60,19 +69,26 @@ basic.forever(function () {
     } else if (snekDirection == 270) {
         snekXPos.unshift(Math.constrain(snekXPos[0] - 1, 0, 4))
         snekYPos.unshift(snekYPos[0])
-    } else {
+    } else if (snekDirection == 0 || snekDirection == 360) {
         snekYPos.unshift(Math.constrain(snekYPos[0] - 1, 0, 4))
         snekXPos.unshift(snekXPos[0])
     }
     if (snekXPos[0] == appleX && snekYPos[0] == appleY) {
         snekLength += 1
-        do{
-         appleY = randint(0, 4)
-         appleX = randint(0, 4)
-        }while(!contains(snekXPos,appleX)&&!contains(snekYPos,appleY));
-newAppPos = true
+        appleX = randint(0, 4)
+        appleY = randint(0, 4)
+        while (!(contains(snekYPos, appleY)) && !(contains(snekXPos, appleX))) {
+            appleX = randint(0, 4)
+            appleY = randint(0, 4)
+        }
+        newAppPos = true
     } else {
         newAppPos = false
+        if (snekLength == 1 && (snekXPos[0] == snekXPos[1] && snekYPos[0] == snekYPos[1])) {
+            hit_wall = true
+        } else {
+            hit_wall = false
+        }
         snekXPos.pop()
         snekYPos.pop()
     }
@@ -85,7 +101,7 @@ newAppPos = true
         startGame()
     } else {
         drawSnek(newAppPos)
-        if (contains(snekXPos.slice(1), snekXPos[0]) && contains(snekYPos.slice(1), snekYPos[0])) {
+        if (snakeColidingWithBody(snekXPos.slice(1), snekYPos.slice(1), snekXPos[0], snekYPos[0]) || hit_wall) {
             basic.clearScreen()
             music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 4610, 1, 255, 255, 1000, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.InBackground)
             while (!(input.buttonIsPressed(Button.AB))) {
